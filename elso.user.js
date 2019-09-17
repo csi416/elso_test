54bbf533c95c55084bade418a710ba08a032a882
csi416 <55428926+csi416@users.noreply.github.com>
2019-09-17 11:31:22 +0200

    adding elso.user.js
    blablabla

---
+++ elso.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        csi416_test
+// @namespace   csi416_test_namespace
+// @description csi416_test_description
+// @include     *index.hu*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+alert("indexet olvasol dikmáhe?!");
